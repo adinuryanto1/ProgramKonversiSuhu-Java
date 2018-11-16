7faad053825cf5af3b39720e8eecc5cd09beb111
adinuryanto1 <adinuryanto14@mhs.pelitabangsa.ac.id>
2018-11-16 22:26:00 +0700

    v-01

---
+++ src/konversi/fahreinheit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konversi;
+
+/**
+ *
+ * @author user
+ */
+public class fahreinheit {
+    public double toCalvin(double n){
+        double k;
+        k = (n + 456.67)/1.8;
+        return k ;
+    }public double toFahreinheit (double n){
+        double k;
+        k = n;
+        return k ;
+    }public double toCelcius(double n){
+        double k;
+        k = (n -32)/1.8;
+        return k ;
+    }
+}
